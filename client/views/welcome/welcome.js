4acb8e37dbe7f1623807b68334cc7e1a9fea32f5
Carlos Cervantes <CCER562@yahoo.com>
2013-12-08 14:28:21 -0800

    merge fix

--- client/views/welcome/welcome.js
+++ client/views/welcome/welcome.js
@@ -6,27 +6,3 @@ Template.welcome.events({
 		window.location = "/innout?min=" + minutes;
   }
 });
-
-jQuery(document).ready(function($) {
-	var isWelcomePage = $("div#page-welcome").length > 0;
-	if(isWelcomePage) {
-		document.getElementById('txtMinutes').focus();
-<<<<<<< HEAD
-		// $("a#btnGetStarted").bind("click", function(e, sender) {
-		// 	var minutes = $("#txtMinutes").val();
-		// 	console.log("Start working out for " + minutes);
-		// 	setCookie("minutes", minutes);
-		// 	window.location = "/innout?min=" + minutes;
-		// 	//return true;
-		// });
-=======
-		$("a#btnGetStarted").bind("click", function(e, sender) {
-			var minutes = $("#txtMinutes").val();
-			console.log("Start working out for " + minutes);
-			Session.set("minutes", minutes);
-			window.location = "/innout"
-			//return true;
-		});
->>>>>>> 37c230f4d86b6d0a64e57829f663cf5ef34ac05b
-	}
-});
