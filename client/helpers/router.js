ecd53688edd49b8427cf3fe98b30287bdca387be
Carlos Cervantes <CCER562@yahoo.com>
2013-12-07 19:32:23 -0800

    Added basic excercises page and improved styles

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -2,6 +2,7 @@ Meteor.Router.add({
   //'/': 'postsList',
   '/': 'welcome',
   '/innout': 'innout',
+  '/excercise': 'excercise',
   '/posts/_id': {
   	to: 'postPage',
   	and: function(id) { Session.set('currentPostId', id); }
---
+++ client/views/excercise/excercise.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+// 	alert("Lets Excercise")
+
+// })
+
+// var program = function(minutes) {
+// 	var indoor = [
+// 		{"0", ""}
+// 	]
+
+// 	function tick = function() {
+
+// 	};
+
+// };
